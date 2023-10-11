fa785f70c8f79947d2404c452d0f9fcf078ca3e3
Alperen <129869740+Alperenozcn@users.noreply.github.com>
2023-10-11 23:58:21 +0300

    Add project files.

---
+++ BlogApp/Data/BlogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Properties.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<BlogCategory> BlogCategories { get; set; }
+    }
+}
